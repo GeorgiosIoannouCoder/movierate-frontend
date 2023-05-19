8887d680e5dc12cf8258f793c150e2de7dcba94a
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2023-05-19 13:32:50 -0400

    update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://movierate-backend-k389l.ondigitalocean.app/api",
 });
 
 export default client;
