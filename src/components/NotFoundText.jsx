140b1bf0087ace1764625a96a23c0f245a2aec60
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2023-05-19 15:59:47 -0400

    update text font

--- src/components/NotFoundText.jsx
+++ src/components/NotFoundText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NotFoundText({ text, visible }) {
   if (!visible) return null;
   return (
-    <h1 className="font-semibold text-3xl text-secondary dark:text-custom-gray text-center py-5 opacity-40">
+    <h1 className="font-semibold text-3xl text-secondary dark:text-custom-gray text-center py-5 opacity-40 font-mono">
       {text}
     </h1>
   );
--- src/components/RatingStar.jsx
+++ src/components/RatingStar.jsx
@@ -4,11 +4,13 @@ import { AiFillStar } from "react-icons/ai";
 export default function RatingStar({ rating }) {
   if (!rating)
     return (
-      <p className="text-highlight dark:text-highlight-dark">No reviews</p>
+      <p className="text-highlight dark:text-highlight-dark font-mono">
+        No reviews
+      </p>
     );
 
   return (
-    <p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
+    <p className="text-highlight dark:text-highlight-dark flex items-center space-x-1 font-mono">
       <span>{rating}</span>
       <AiFillStar />
     </p>
