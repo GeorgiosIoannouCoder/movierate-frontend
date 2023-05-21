9a49a4bc476129c37fc227284e7fe128df69ac6c
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2023-05-21 04:21:44 -0400

    update Selector.jsx

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Selector({ name, options, value, label, onChange }) {
   return (
     <select
-      className="border-2  dark:bg-custom-gray dark:border-custom-gray border-secondary bg-secondary dark:focus:border-custom-gold focus:border-custom-gold dark:hover:border-custom-gold hover:border-custom-gold p-1 pr-10 outline-none transition rounded bg-transparent text-custom-gray dark:text-primary dark:focus:text-primary focus:text-custom-gray font-mono"
+      className="border-2  dark:bg-custom-gray dark:border-primary bg-black  dark:focus:border-custom-gold focus:border-custom-gold dark:hover:border-custom-gold hover:border-custom-gold p-1 pr-10 outline-none transition rounded-lg bg-transparent text-custom-gray dark:text-primary dark:focus:text-primary font-mono"
       id={name}
       name={name}
       value={value}
