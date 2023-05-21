ab25a99456e6617d8f36219f9c0f5ae73708df19
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2023-05-21 00:19:02 -0400

    update theme.js

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,4 +2,4 @@ export const commonModalClasses =
   "dark:bg-secondary bg-white drop-shadow-lg rounded p-6 space-y-6";
 
 export const commonInputClasses =
-  "w-full bg-transparent outline-none dark:border-custom-gray border-primary dark:focus:border-white focus:border-secondary transition dark:text-custom-gray text-primary";
+  "w-full bg-transparent outline-none dark:border-custom-gray border-primary dark:focus:border-custom-gold focus:border-custom-gold transition dark:text-custom-gray text-primary fonr-mono";
